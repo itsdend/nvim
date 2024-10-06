f9c18934e2a9e15245e42b964450a70132fa2766
itsdend <marko.samarzija1@gmail.com>
2024-10-06 04:59:41 +0200

    removed print function

--- after/syntax/erlang.lua
+++ after/syntax/erlang.lua
@@ -2,7 +2,6 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = "erlang",
     callback = function()
         vim.opt_local.conceallevel = 1  -- Set conceal level to 2
-		vim.api.nvim_echo({{"Erlang syntax loaded!", "MoreMsg"}}, false, {})
     end,
 })
 
